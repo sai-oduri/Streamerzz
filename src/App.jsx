6a7c5d6913139f18b601afe3646c33feda285eae
sai-oduri <saioduri95@gmail.com>
2025-02-01 23:33:57 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <main className="text-sm text-neutral-300 antialiased">
       <Navbar />
+      <HeroSection />
     </main>
   )
 }
