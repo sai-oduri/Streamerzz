20c1762296e0ccb8c16a86d068eab26392fe8232
sai-oduri <saioduri95@gmail.com>
2025-02-02 12:19:39 +0530

    features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import HeroSection from "./components/HeroSection"
 import HowItWorks from "./components/HowItWorks"
+import KeyFeatures from "./components/KeyFeatures"
 import Navbar from "./components/Navbar"
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <HeroSection />
       <HowItWorks />
+      <KeyFeatures />
     </main>
   )
 }
